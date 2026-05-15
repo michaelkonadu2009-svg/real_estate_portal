8fa299f02f95ef00c183c3d769ca508f225f11e2
michaelkonadu2009-svg <michaelkonadu2009@gmail.com>
2026-05-14 21:51:14 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,14 @@
 <?php
+// Loads the config file and starts the session if needed
 require_once 'config/config.php';
+
+// Clears all session variables
 session_unset();
+
+// Destroys the current session
 session_destroy();
+
+// Sends the user back to the homepage
 header('Location: index.php');
 exit;
 ?>
